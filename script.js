a69aec1a90d4de61c74c9748ede4fbcf7d2b40e5
LittleAkira <cinzia.ariotti.10@gmail.com>
2022-05-02 12:37:11 +0200

    update

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 //CREAZIONE OBJECT SENZA LA CLASS
-const grades = [8,3,8,6];
+// const grades = [8,3,8,6];
 
-const student1 = {
-    name: "Cinzia",
-    surname: "Ariotti",
-    yob: 1996,
-    gender: "F",
-    gradeArray: grades,
-};
+// const student1 = {
+//     name: "Cinzia",
+//     surname: "Ariotti",
+//     yob: 1996,
+//     gender: "F",
+//     gradeArray: grades,
+// };
 
 //FUNZIONE PER CALCOLARE IL VOTO MINIMO
 // function calculateStudentMin(student){
@@ -26,5 +26,16 @@ const student1 = {
 
 //CREAZIONE OBJECT CON LA CLASS
 
-const student = new Student('Cinzia', 'Ariotti', 1996, 'F', grades);
-console.log(student);
+// const grades = [8,3,8,6];
+// const student = new Student('Cinzia', 'Ariotti', 1996, 'F', grades);
+
+const student1 = new Student('Cinzia', 'Ariotti', 1996, 'F', [8,10,3]);
+
+// console.log(student);
+// console.log(student.calculateStudentMin());
+
+console.log(student1.toString());
+
+// const student2 = new Student('Paolo', 'Foppiano', 1997, 'M', [10,9,2]);
+
+// console.log(student2.calculateStudentMin(), student2.calculateAge());
--- student.js
+++ student.js
@@ -9,15 +9,33 @@ class Student{
         this.gradesArray = gradeArray;
     }
 
-};
+    //FUNZIONE PER CALCOLARE LA MEDIA DEI VOTI
+    calculateStudentMin() {
+        let sum = 0;
+        for (let i = 0; i < this.gradesArray.length; i++) {
+            const grade = this.gradesArray[i];
+            sum = sum + grade;
+        }
+        const min = sum / this.gradesArray.length;
+        return min;
+    };
 
+    //FUNZIONE PER CALCOLARE L'ETA'
+    calculateAge(){
+        const now = new Date();
+        const year = now.getFullYear();
+        const age = year-this.yob;
+        return age;
+    };
 
-calculateStudentMin() {
-    let sum = 0;
-    for (let i = 0; i < this.gradesArray.length; i++) {
-        const grade = this.gradeArray[i];
-        sum = sum + grade;
+    //FUNZIONE PER TRASFORMARE L'OBJECT IN STRINGA
+    toString(){
+        const genere = this.gender === "F"?"Femmina":"Maschio";
+        const studentString =   "Nome: " + this.name + "\n" + 
+                                "Cognome: " + this.surname + "\n" + 
+                                "Età: " + this.calculateAge() + "\n" + 
+                                "Genere: " + genere + "\n" +
+                                "Media voti: " + this.calculateStudentMin();
+        return studentString;
     }
-    const min = sum / student.gradeArray.length;
-    return min;
-};
+}
